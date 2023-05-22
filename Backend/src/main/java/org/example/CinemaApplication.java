67f1b95d04c1aaf59943771d1f0487757d077da4
Karolina936 <75604545+Karolina936@users.noreply.github.com>
2023-05-22 19:25:48 +0200

    liquibase

--- Backend/src/main/java/org/example/CinemaApplication.java
+++ Backend/src/main/java/org/example/CinemaApplication.java
@@ -1,6 +1,7 @@
 package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CinemaApplication {
@@ -8,5 +9,4 @@ public class CinemaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
-
 }
